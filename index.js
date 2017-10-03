7d0ecc08b14694b9d149651078d9a7f4167ff3f6
aaron naeger <aaronmixes@gmail.com>
2017-10-03 01:43:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ if (name==="Susan"){
 }
 
 
-if (height===74){
+if (height===70){
     console.log("The height is correct")
 } else {
-    console.log("Expected the height to be 74")
+    console.log("Expected the height to be 70")
 }
